28cd7f7845cb65c27ea33f6b40d85a57035ae69a
hmateu <hmateu.ortola@gmail.com>
2023-08-07 19:33:05 +0200

    feat: Crea el CRUD de Categoría

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Entity\Category;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,28 +10,28 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Serializer\SerializerInterface;
 
-class UserController extends AbstractController
+class CategoryController extends AbstractController
 {
-    #[Route('/users', name: 'get_all_users', methods: ['GET', 'HEAD'])]
+    #[Route('/categories', name: 'get_all_categories', methods: ['GET', 'HEAD'])]
     public function list(ManagerRegistry $doctrine, SerializerInterface $serializer): JsonResponse
     {
         try {
-            $users = $doctrine->getRepository(User::class)->findAll();
+            $categories = $doctrine->getRepository(Category::class)->findAll();
 
-            //Realiza la serialización de los objetos de la colección $users en formato JSON
-            $usersAsJson = $serializer->serialize($users, 'json', ['groups' => 'user']);
+            //Realiza la serialización de los objetos de la colección $categories en formato JSON
+            $categoriesAsJson = $serializer->serialize($categories, 'json', ['groups' => 'category']);
 
             // Convierte la cadena JSON a un array asociativo
-            $usersArray = json_decode($usersAsJson, true);
+            $categoriesArray = json_decode($categoriesAsJson, true);
 
-            // Crea el array de respuesta que contiene el campo 'success' => true junto con la lista de usuarios
+            // Crea el array de respuesta que contiene el campo 'success' => true junto con la lista de categorías
             $responseArray = [
                 'success' => true,
-                'data' => $usersArray
+                'data' => $categoriesArray
             ];
 
             // Convierte el array de respuesta a una cadena JSON
@@ -41,31 +41,31 @@ class UserController extends AbstractController
         } catch (\Throwable $th) {
             return new JsonResponse([
                 'success' => false,
-                'message' => 'Error al mostrar los usuarios: ' . $th->getMessage()
+                'message' => 'Error al mostrar las categorías: ' . $th->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    #[Route('/user/{id}', name: 'get_user_by_id', methods: ['GET'])]
+    #[Route('/category/{id}', name: 'get_category_by_id', methods: ['GET'])]
     public function show(ManagerRegistry $doctrine, SerializerInterface $serializer, int $id): Response
     {
         try {
-            $user = $doctrine->getRepository(User::class)->find($id);
+            $category = $doctrine->getRepository(Category::class)->find($id);
 
-            if (!$user) {
+            if (!$category) {
                 return new JsonResponse([
                     'success' => true,
-                    'message' => 'No se ha encontrado ningún usuario con el id ' . $id
+                    'message' => 'No se ha encontrado ninguna categoría con el id ' . $id
                 ], Response::HTTP_NOT_FOUND);
             }
 
-            $usersAsJson = $serializer->serialize($user, 'json', ['groups' => 'user']);
+            $categoriesAsJson = $serializer->serialize($category, 'json', ['groups' => 'category']);
 
-            $usersArray = json_decode($usersAsJson, true);
+            $categoriesArray = json_decode($categoriesAsJson, true);
 
             $responseArray = [
                 'success' => true,
-                'data' => $usersArray
+                'data' => $categoriesArray
             ];
 
             $responseJson = json_encode($responseArray, JSON_UNESCAPED_UNICODE);
@@ -74,13 +74,13 @@ class UserController extends AbstractController
         } catch (\Throwable $th) {
             return new JsonResponse([
                 'success' => false,
-                'message' => 'Error al mostrar el usuario: ' . $th->getMessage()
+                'message' => 'Error al mostrar la categoría: ' . $th->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    #[Route('/new-user', name: 'register_user', methods: ['POST'])]
-    public function createUser(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
+    #[Route('/new-category', name: 'create_category', methods: ['POST'])]
+    public function createCategory(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
     {
         try {
             $jsonData = $request->getContent();
@@ -98,25 +98,15 @@ class UserController extends AbstractController
                         'message' => 'Formato de nombre no válido.'
                     ])
                 ],
-                'surname' => [
+                'description' => [
                     new Assert\NotBlank([
-                        'message' => 'El campo apellidos no puede estar vacío.'
+                        'message' => 'El campo descripción no puede estar vacío.'
                     ]),
                     new Assert\Regex([
-                        'pattern' => '/^(?=(?:[^A-Za-z]*[A-Za-z]){2})(?!(?:[^A-Za-z]*[A-Za-z]){21})[A-Za-z ]{2,20}$/
+                        'pattern' => '/^(?!\s)(?! *$)[A-Za-z0-9 ]{10,255}$/
                                     ',
                         'match' => true,
-                        'message' => 'Formato de apellidos no válido.'
-                    ])
-                ],
-                'email' => [
-                    new Assert\NotBlank([
-                        'message' => 'El campo email no puede estar vacío.'
-                    ]),
-                    new Assert\Regex([
-                        'pattern' => '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
-                        'match' => true,
-                        'message' => 'Formato de email no válido.'
+                        'message' => 'Formato de descripción no válido.'
                     ])
                 ]
             ]);
@@ -133,8 +123,8 @@ class UserController extends AbstractController
                 ], Response::HTTP_BAD_REQUEST);
             }
 
-            $user = $serializer->deserialize($jsonData, User::class, 'json');
-            $errors = $validator->validate($user);
+            $category = $serializer->deserialize($jsonData, Category::class, 'json');
+            $errors = $validator->validate($category);
 
             if (count($errors) > 0) {
                 $errorMessages = [];
@@ -149,97 +139,96 @@ class UserController extends AbstractController
 
             $entityManager->getConnection()->beginTransaction();
 
-            $entityManager->persist($user);
+            $entityManager->persist($category);
             $entityManager->flush();
 
             $entityManager->getConnection()->commit();
 
             $data =  [
-                'id' => $user->getId(),
-                'name' => $user->getName(),
-                'surname' => $user->getSurname(),
-                'email' => $user->getEmail()
+                'id' => $category->getId(),
+                'name' => $category->getName(),
+                'description' => $category->getDescription()
             ];
 
             return new JsonResponse([
                 'success' => true,
-                'message' => 'Usuario creado satisfactoriamente',
+                'message' => 'Categoría creada satisfactoriamente',
                 'data' => $data
             ], Response::HTTP_OK);
         } catch (\Throwable $th) {
             $entityManager->getConnection()->rollBack();
             return new JsonResponse([
                 'success' => false,
-                'message' => 'Error al registrar el usuario: ' . $th->getMessage()
+                'message' => 'Error al crear la categoría: ' . $th->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    #[Route('/user/{id}', name: 'modify_user', methods: ['PUT', 'PATCH'])]
+    #[Route('/category/{id}', name: 'modify_category', methods: ['PUT', 'PATCH'])]
     public function update(ManagerRegistry $doctrine, Request $request, int $id, SerializerInterface $serializer): JsonResponse
     {
         try {
             $entityManager = $doctrine->getManager();
 
-            $user = $entityManager->getRepository(User::class)->find($id);
+            $category = $entityManager->getRepository(Category::class)->find($id);
 
-            if (!$user) {
+            if (!$category) {
                 return new JsonResponse([
                     'success' => true,
-                    'message' => 'No se ha encontrado ningún usuario con el id ' . $id
+                    'message' => 'No se ha encontrado ninguna categoría con el id ' . $id
                 ], Response::HTTP_NOT_FOUND);
             }
 
             $data = json_decode($request->getContent(), true);
 
-            $serializer->deserialize(json_encode($data), User::class, 'json', ['object_to_populate' => $user]);
+            $serializer->deserialize(json_encode($data), Category::class, 'json', ['object_to_populate' => $category]);
 
             $entityManager->flush();
 
-            $responseData = $serializer->serialize($user, 'json');
+            $responseData = $serializer->serialize($category, 'json');
 
             return new JsonResponse([
                 'success' => true,
-                'message' => 'Modificado el usuario con id ' . $id,
+                'message' => 'Modificada la categoría con id ' . $id,
                 'data' => json_decode($responseData, true)
             ], Response::HTTP_OK);
         } catch (\Throwable $th) {
             return new JsonResponse([
                 'success' => false,
-                'message' => 'Error al modificar el usuario: ' . $th->getMessage()
+                'message' => 'Error al modificar la categoría: ' . $th->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
-    #[Route('/user/{id}', name: 'delete_user', methods: ['DELETE'])]
+    #[Route('/category/{id}', name: 'delete_category', methods: ['DELETE'])]
     public function delete(ManagerRegistry $doctrine, int $id, SerializerInterface $serializer): JsonResponse
     {
         try {
             $entityManager = $doctrine->getManager();
-            $userRepository = $entityManager->getRepository(User::class);
+            $categoryRepository = $entityManager->getRepository(User::class);
 
-            $user = $userRepository->find($id);
+            $category = $categoryRepository->find($id);
 
-            if (!$user) {
+            if (!$category) {
                 return new JsonResponse([
                     'success' => true,
-                    'message' => 'No se ha encontrado ningún usuario con el id ' . $id
+                    'message' => 'No se ha encontrado ninguna categoría con el id ' . $id
                 ], Response::HTTP_NOT_FOUND);
             }
 
-            $entityManager->remove($user);
+            $entityManager->remove($category);
             $entityManager->flush();
 
             $responseData = $serializer->serialize([
                 'success' => true,
-                'message' => 'Eliminado usuario con id ' . $id
+                'message' => 'Eliminada categoría con id ' . $id
             ], 'json');
 
             return new JsonResponse($responseData, Response::HTTP_OK, [], true);
         } catch (\Throwable $th) {
             return new JsonResponse([
                 'success' => false,
-                'message' => 'Error al eliminar el usuario: ' . $th->getMessage()
+                'message' => 'Error al eliminar la categoría: ' . $th->getMessage()
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,8 +6,10 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[UniqueEntity('name', message: 'Esta categoría ya existe.')]
 class Category
 {
     #[ORM\Id]
